663bd294d5490b4eee2c98483a599d6e99cc5f43
skazzp <skazzp@gmail.com>
2022-07-09 00:28:55 +0300

    HAMBURGER

--- src/js/mobile-menu.js
+++ src/js/mobile-menu.js
@@ -3,9 +3,10 @@
   const mobileMenuRef = document.querySelector('[data-menu]');
 
   menuBtnRef.addEventListener('click', () => {
-    const expanded = menuBtnRef.getAttribute('aria-expanded') === 'true' || false;
+    const expanded =
+      menuBtnRef.getAttribute('aria-expanded') === 'true' || false;
 
-    menuBtnRef.classList.toggle('is-open');
+    menuBtnRef.classList.toggle('is-active');
     menuBtnRef.setAttribute('aria-expanded', !expanded);
 
     mobileMenuRef.classList.toggle('is-open');

aa0215024433c4f0d815d6445699cdf6354fd709
Vitaliy Opanashchuk <booking.opanashchuk@gmail.com>
2022-07-12 14:20:04 +0300

    slider-need-animation

--- src/js/slick.js
+++ src/js/slick.js
@@ -4,47 +4,58 @@ $('.reviews__slider').slick({
   arrows: false,
   slidesToShow: 3,
   slidesToScroll: 1,
-  focusOnSelect: true,
+  //   focusOnSelect: true,
   centerMode: true,
+  //   centerPadding: '0px',
+  variableWidth: true,
+  dots: false,
+  mobileFirst: true,
+  //   adaptiveHeight: true,
+  asNavFor: '.reviews__description',
 
   responsive: [
     {
-      breakpoint: 1280,
-      settings: {
-        slidesToShow: 3,
-        slidesToScroll: 1,
-        infinite: true,
-      },
+      //   breakpoint: 1280,
+      //   settings: {
+      //     slidesToShow: 3,
+      //     slidesToScroll: 1,
+      //   },
     },
     {
       breakpoint: 768,
       settings: {
         slidesToShow: 3,
         slidesToScroll: 1,
-        dots: true,
+        dots: false,
       },
     },
     {
       breakpoint: 480,
       settings: {
-        slidesToShow: 3,
+        slidesToShow: 1,
         slidesToScroll: 1,
-        dots: true,
+        dots: false,
       },
     },
   ],
 });
-$('.reviews__slider').on('beforeChange', (e, slick, currSlide, nextSlide) => {
-  if (currSlide === nextSlide) {
-    return;
-  }
-  $('[data-reviews]').addClass('big');
-  $(`[data-slide]`).each((_, slideEl) => {
-    const $slideEl = $(slideEl);
-    console.log($slideEl);
-    const currentSlide = $slideEl.attr('data-slide');
-    $slideEl.toggleClass('big', +currentSlide === nextSlide);
-    $('[data-reviews]').removeClass('big');
-  });
-  //   currSlide.addClass('big');
+$('.reviews__description').slick({
+  asNavFor: '.reviews__slider',
+  dots: true,
+  arrows: false,
+  infinite: true,
 });
+// $('.reviews__slider').on('beforeChange', (e, slick, currSlide, nextSlide) => {
+//   if (currSlide === nextSlide) {
+//     return;
+//   }
+//   $('[data-reviews]').addClass('big');
+//   $(`[data-slide]`).each((_, slideEl) => {
+//     const $slideEl = $(slideEl);
+//     console.log($slideEl);
+//     const currentSlide = $slideEl.attr('data-slide');
+//     $slideEl.toggleClass('big', +currentSlide === nextSlide);
+//     $('[data-reviews]').removeClass('big');
+//   });
+//   //   currSlide.addClass('big');
+// });

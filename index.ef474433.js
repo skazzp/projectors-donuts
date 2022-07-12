47e148c4b1ec2bf41ee38d1540ff72c592b19c46
skazzp <102911058+skazzp@users.noreply.github.com>
2022-07-12 13:32:51 +0000

    Deploying to gh-pages from @ skazzp/projectors-donuts@46c9f8407453de76bc35b711b46b7e4c6974fd02 🚀

---
+++ index.ef474433.js
@@ -0,0 +1,2 @@
+const e=document.querySelectorAll('a[href*="#"]');for(let t of e)t.addEventListener("click",(function(e){e.preventDefault();const o=t.getAttribute("href");document.querySelector(""+o).scrollIntoView({behavior:"smooth",block:"start"})}));
+//# sourceMappingURL=index.ef474433.js.map

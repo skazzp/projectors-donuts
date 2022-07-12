bd5c98048a859967ad2f29ed2729b99dfd0ab1fa
skazzp <skazzp@gmail.com>
2022-07-12 12:03:47 +0300

    js for mob menu

--- src/js/mobile-menu.js
+++ src/js/mobile-menu.js
@@ -1,7 +1,7 @@
 (() => {
   const menuBtnRef = document.querySelector('[data-menu-button]');
   const mobileMenuRef = document.querySelector('[data-menu]');
-  const header = document.querySelector('#header');
+  const menuLinkRef = document.querySelector('#menu');
   const body = document.querySelector('body');
 
   menuBtnRef.addEventListener('click', () => {
@@ -13,5 +13,22 @@
 
     mobileMenuRef.classList.toggle('is-open');
     body.classList.toggle('no-scroll');
+  });
+  menuLinkRef.addEventListener('click', () => {
+    const expanded =
+      menuBtnRef.getAttribute('aria-expanded') === 'true' || false;
+
+    menuBtnRef.classList.toggle('is-active');
+    menuBtnRef.setAttribute('aria-expanded', !expanded);
+
+    mobileMenuRef.classList.toggle('is-open');
+    body.classList.toggle('no-scroll');
+  });
+  window.matchMedia('(min-width: 1280px)').addEventListener('change', e => {
+    if (!e.matches) return;
+
+    mobileMenuRef.classList.toggle('is-open');
+    menuBtnRef.classList.toggle('is-active');
+    menuBtnRef.setAttribute('aria-expanded', false);
   });
 })();
